55e666ce67dfc6680ee06bb3695edf30bca39991
Trevor Stinson <trevor@trevorstinson.com>
2020-08-22 13:32:56 -0500

    Add numpad

--- keyboards/v60_type_r/keymaps/trevorstinson/config.h
+++ keyboards/v60_type_r/keymaps/trevorstinson/config.h
@@ -23,5 +23,5 @@
 #endif
 
 #define COMBO_COUNT 6
-#define TAPPING_TERM 200
-#define COMBO_TERM 200
+#define TAPPING_TERM 300
+#define COMBO_TERM 300
--- keyboards/v60_type_r/keymaps/trevorstinson/keymap.c
+++ keyboards/v60_type_r/keymaps/trevorstinson/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
 	       KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \
 	       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_UP,   \
-	       MO(2), KC_LALT, KC_LGUI,          KC_SPC,                                      KC_RGUI,   KC_LEFT, KC_DOWN,  KC_RIGHT),
+	       LT(2, KC_ESC), KC_LALT, KC_LGUI,          LT(3, KC_SPC),                                      KC_RGUI,   KC_LEFT, KC_DOWN,  KC_RIGHT),
 
 
   [1] = LAYOUT_60_ansi(
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
 	       KC_LCTRL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \
 	       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_UP,   \
-	       MO(2), KC_LGUI, KC_LALT,          KC_SPC,                                      KC_RGUI,   KC_LEFT, KC_DOWN,  KC_RIGHT),
+	       LT(2, KC_ESC), KC_LGUI, KC_LALT,          LT(3, KC_SPC),                                      KC_RGUI,   KC_LEFT, KC_DOWN,  KC_RIGHT),
 
   /* Keymap 1: FN Layer
    * ,-----------------------------------------------------------.
@@ -67,6 +67,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	       KC_TRNS,   KC_TRNS,  KC_UP,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_UP,    KC_TRNS,          KC_INS, \
 	       KC_TRNS,   CMB_TOG,  CMB_OFF,  CMB_ON, RGB_TOG,  BL_OFF,  KC_TRNS,   KC_TRNS, KC_HOME, KC_PGUP, KC_LEFT, KC_RIGHT,          KC_TRNS,          \
 	       KC_TRNS,   DF(0),  DF(1),  KC_TRNS,  KC_TRNS,  KC_VOLD,   KC_VOLU, KC_MUTE, KC_END,  KC_PGDN,  KC_DOWN,           KC_TRNS,  \
+	       KC_TRNS,   KC_TRNS,  KC_TRNS,            KC_TRNS,                                                           KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS),
+
+  /* Keymap 3: Numpad Layer
+   * ,-----------------------------------------------------------.
+   * |`  | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|  Del  |
+   * |-----------------------------------------------------------|
+   * |     |   |Up |   |   |   |   |   |Prt|Scl|Pau|Up|   | Ins  |
+   * |-----------------------------------------------------------|
+   * |      |Lft|Dwn|Rig|   |   |   |   |Hme|PgU|Lef|Rig|        |
+   * |-----------------------------------------------------------|
+   * |        |   |   |   |   |VolD|VolU|Mut|End|PgD|Dwn|        |
+   * |-----------------------------------------------------------|
+   * |    |    |    |                        |    |    |    |    |
+   * `-----------------------------------------------------------'
+   */
+  [3] = LAYOUT_60_ansi(
+	       KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_7,   KC_8,   KC_9,   KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS, \
+	       KC_TRNS,   KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_4,   KC_5,   KC_6, KC_TRNS, KC_TRNS,    KC_TRNS,          KC_TRNS, \
+	       KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_1,   KC_2,   KC_3, KC_TRNS, KC_TRNS,          KC_TRNS,          \
+	       KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_0, KC_TRNS,  KC_TRNS,  KC_TRNS,           KC_TRNS,  \
 	       KC_TRNS,   KC_TRNS,  KC_TRNS,            KC_TRNS,                                                           KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS),
 
 };
