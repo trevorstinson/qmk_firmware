7810aca1a53b2aee7520f70dafcd942c5fa6484e
Trevor Stinson <trevor@trevorstinson.com>
2020-09-02 14:29:40 -0500

    Add numpad layer

--- keyboards/dztech/dz65rgb/keymaps/trevorstinson/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/trevorstinson/keymap.c
@@ -7,7 +7,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_MUTE,
         KC_LCTL,        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_VOLU,
         KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_VOLD,
-        LT(2, KC_ESC),  KC_LALT, KC_LGUI,                            KC_SPC,                    KC_RGUI, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        LT(2, KC_ESC),  KC_LALT, KC_LGUI,                            KC_SPC,                    KC_RGUI, KC_RALT, TG(3),   KC_LEFT, KC_DOWN, KC_RGHT
     ),
     // Windows layer
     [1] = LAYOUT_65_ansi(
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_MUTE,
         KC_LCTL,        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_VOLU,
         KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_VOLD,
-        LT(2, KC_ESC),  KC_LCTL, KC_LALT,                            KC_SPC,                    KC_RGUI, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        LT(2, KC_ESC),  KC_LCTL, KC_LALT,                            KC_SPC,                    KC_RGUI, KC_RALT, TG(3),   KC_LEFT, KC_DOWN, KC_RGHT
     ),
     // Function layer
     [2] = LAYOUT_65_ansi(
@@ -24,5 +24,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,        RGB_SPI, RGB_SPD, _______, _______, _______, _______, _______, _______, _______, _______, _______,          EEP_RST,        KC_PGDN,
         KC_LSFT,                 DF(0),   DF(1),   _______, _______, _______, NK_TOGG, _______, _______, _______, _______, _______, LCTL(KC_HOME),  KC_MUTE,
         _______,        _______, _______,                            _______,                   _______, _______, _______, KC_HOME, LCTL(KC_END),   KC_END
+    ),
+    // Numpad layer
+    [3] = LAYOUT_65_ansi(
+        KC_ESC,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,  KC_9,    KC_0,    XXXXXXX, XXXXXXX, KC_BSPC, KC_DEL,
+        KC_TAB,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_4,    KC_5,  KC_6,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_1,    KC_2,  KC_3,    XXXXXXX, XXXXXXX,          KC_ENT,  XXXXXXX,
+        KC_LSFT,                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_0,  KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX, KC_UP,   XXXXXXX,
+        XXXXXXX,        XXXXXXX, XXXXXXX,                            KC_SPC,                  XXXXXXX, XXXXXXX,  TG(3),  KC_LEFT, KC_DOWN, KC_RGHT
     )
 };
